91108a8adac15b6d3de814a70a75e72d9f6aa91c
Luca <lucasacramento32@gmail.com>
2026-08-02 20:34:34 -0300

    feat: add id in some dto returns

--- src/main/java/com/foody/delivery/domain/order/OrderItemResponseDTO.java
+++ src/main/java/com/foody/delivery/domain/order/OrderItemResponseDTO.java
@@ -1,9 +1,13 @@
 package com.foody.delivery.domain.order;
 
+import java.math.BigDecimal;
+
 public record OrderItemResponseDTO(
-        Integer quantity
+        Integer quantity,
+        String item_name,
+        BigDecimal item_price
 ) {
     public OrderItemResponseDTO(OrderItem orderItem) {
-        this(orderItem.getQuantity());
+        this(orderItem.getQuantity(), orderItem.getItem().getItem_name(), orderItem.getUnit_price());
     }
 }
